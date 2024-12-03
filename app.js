6442634fa255d2361311cc9b235c6260d4cfd940
ALI HUZAIFA KHAN <alikhan365w2@gmail.com>
2024-12-03 18:27:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Initialize todos array from localStorage
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+var todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 // Save todos to localStorage
 function saveTodos() {
@@ -8,29 +8,30 @@ function saveTodos() {
 
 // Render todos
 function renderData() {
-  const todoList = document.getElementById('todoList');
+  var todoList = document.getElementById('todoList');
   todoList.innerHTML = '';
   todos = JSON.parse(localStorage.getItem('todos')) || [];
     
-  todos.forEach((todo, index) => {
-    const li = document.createElement('li');
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+    var li = document.createElement('li');
     li.className = todo.done ? 'todo-item completed' : 'todo-item';
-      
+    
     li.innerHTML = `
       <span>${todo.text}</span>
       <div>
-        <button onclick="markAsDone(${index})" ${todo.done ? 'disabled' : ''}>Done</button>
-        <button onclick="editTodo(${index})" ${todo.done ? 'disabled' : ''}>Edit</button>
-        <button onclick="deleteTodo(${index})">Delete</button>
+        <button onclick="markAsDone(${i})" ${todo.done ? 'disabled' : ''}>Done</button>
+        <button onclick="editTodo(${i})" ${todo.done ? 'disabled' : ''}>Edit</button>
+        <button onclick="deleteTodo(${i})">Delete</button>
       </div>
     `;
     todoList.appendChild(li);
-  });
+  }
 }
 
 // Add todo
 function addTodo(text) {
-  todos.push({ text, done: false });
+  todos.push({ text: text, done: false });
   saveTodos();
   renderData();
 }
@@ -53,8 +54,8 @@ function markAsDone(index) {
 
 // Edit todo
 function editTodo(index) {
-  const li = document.querySelectorAll('.todo-item')[index];
-  const text = todos[index].text;
+  var li = document.getElementsByClassName('todo-item')[index];
+  var text = todos[index].text;
   li.innerHTML = `
     <input type="text" value="${text}">
     <button onclick="saveTodoEdit(${index})">Save</button>
@@ -63,20 +64,20 @@ function editTodo(index) {
 
 // Save todo edit
 function saveTodoEdit(index) {
-  const input = document.querySelector('.todo-item input');
+  var input = document.getElementsByClassName('todo-item')[index].getElementsByTagName('input')[0];
   todos[index].text = input.value;
   saveTodos();
   renderData();
 }
 
 // Handle form submission
-document.getElementById('todoForm').addEventListener('submit', (e) => {
+document.getElementById('todoForm').onsubmit = function(e) {
   e.preventDefault();
-  const input = document.getElementById('todoInput');
-  if (input.value.trim()) {
-    addTodo(input.value.trim());
+  var input = document.getElementById('todoInput');
+  if (input.value) {
+    addTodo(input.value);
     input.value = '';
   }
-});
+};
 
 renderData();
